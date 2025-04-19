85d6e6875362f9fe8609e12a832fdc341feffe67
Christophe CHAUDELET <cho934@users.noreply.github.com>
2025-04-19 16:12:36 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,27 @@
 function Runrun () {
     if (color == 1 || color == 0) {
         if (color == 1) {
-            detection = 0
+            detection = 1
             maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 120)
             basic.pause(7000)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 120)
+            basic.pause(1000)
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 120)
+            basic.pause(2000)
             detection = 0
             maqueen.motorStop(maqueen.Motors.All)
         }
     }
     if (color == 2) {
-        detection = 0
+        detection = 1
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 120)
         basic.pause(7000)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 120)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+        basic.pause(1000)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 120)
+        basic.pause(2000)
         detection = 0
         maqueen.motorStop(maqueen.Motors.All)
     }
@@ -19,12 +29,32 @@ function Runrun () {
 function Endflower2 () {
     bougiewoogie = 1
 }
+function doVL53L1X () {
+    distance = VL53L1X.readSingle()
+    serial.writeValue("dist", distance)
+    if (distance < 70) {
+        serial.writeValue("x", 1)
+        maqueen.motorStop(maqueen.Motors.All)
+    } else {
+    	
+    }
+}
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Skull)
     color = 1
     radio.sendNumber(1)
     radio.sendString("YELLOW")
 })
+function doUltraSonic () {
+    distance = maqueen.Ultrasonic()
+    serial.writeValue("dist", distance)
+    if (distance < 15) {
+        serial.writeValue("x", 1)
+        maqueen.motorStop(maqueen.Motors.All)
+    } else {
+    	
+    }
+}
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Diamond)
     color = 2
@@ -53,6 +83,8 @@ let duration = 85000
 serial.redirectToUSB()
 Maqueen_V5.I2CInit()
 VL53L1X.init()
+VL53L1X.setDistanceMode(VL53L1X.DistanceMode.Short)
+VL53L1X.setMeasurementTimingBudget(50000)
 bougiewoogie = 0
 pins.touchSetMode(TouchTarget.P0, TouchTargetMode.Resistive)
 detection = 0
@@ -90,21 +122,14 @@ basic.forever(function () {
     }
 })
 control.inBackground(function () {
-    while (false) {
+    while (true) {
         if (detection == 1) {
-            distance = maqueen.Ultrasonic()
-            serial.writeValue("dist", distance)
-            if (distance < 15) {
-                serial.writeValue("x", 1)
-                maqueen.motorStop(maqueen.Motors.All)
-            } else {
-            	
-            }
+            doVL53L1X()
         }
         if (bougiewoogie == 1) {
-            maqueen.servoRun(maqueen.Servos.S2, 100)
+            maqueen.servoRun(maqueen.Servos.S2, 110)
             basic.pause(200)
-            maqueen.servoRun(maqueen.Servos.S2, 80)
+            maqueen.servoRun(maqueen.Servos.S2, 60)
             basic.pause(200)
         }
         basic.pause(200)
